558256082c6eed6418f2cd2df3f66dab20090995
amberOoO <51843935+amberOoO@users.noreply.github.com>
2019-06-19 09:02:42 +0800

    更新了gitignore文件

---
+++ env/mysiteEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\amber\documents\github\mysite\env\mysiteenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
